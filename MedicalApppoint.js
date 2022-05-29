8ebcc26eb71a8eac7818db57811327c9f4ff12e8
MaFerAlvaradoS <105398097+MaFerAlvaradoS@users.noreply.github.com>
2022-05-28 23:45:48 -0500

    Actualización Medical Record
    Actualización de la sección Medical Record, incluyendo el botón para regresar a la pantalla anterior

--- MedicalApppoint.js
+++ MedicalApppoint.js
@@ -1,5 +1,8 @@
 import * as React from "react";
-import { Box, Heading, View, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider, Image} from "native-base";
+import {
+    Box, Heading, View, VStack, FormControl, Input, Link,
+    Button, HStack, Center, NativeBaseProvider, Image
+} from "native-base";
 import { useNavigation } from '@react-navigation/native';
 import { Text, StyleSheet, Label } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
@@ -9,86 +12,94 @@ import { DrawerNavigation } from "./Stack";
 
 const Drawer = createDrawerNavigator()
 const MedAp = () => {
+    const navigation = useNavigation();
+    const tab = <>&nbsp;&nbsp;&nbsp;&nbsp;</>;
     return (
-
-
-        
-<Box safeArea p="2" py="8" w="90%" maxW="400">
-
-
-<Heading   size="lg" fontWeight="600"  _dark={{
-            color: "black"
-          }}>
-              Medical appointment.       
+        <Box safeArea p="2" py="8" w="90%" maxW="400">
+            {tab}
+            {tab}
+            {tab}
+            <Heading size="lg" fontWeight="600" _dark={{
+                color: "black"
+            }}>
+                Medical appointment.
             </Heading>
+            {tab}
+            {tab}
+            <Image style={{ width: 350, height: 180, marginBottom: 15 }} source={require("./Imagenes/citas medicas.jpg")} />
+            <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Menu')} >
+                Menu
+            </Button>
+            <Box>
+                <Text>
+                    This section summarizes all the information
+                    about the user's upcoming medical appointments.
+                </Text>
+            </Box>
+            {tab}
+            {tab}
+            <Box>
+                <Text>
+                APPLICANT: Maria Luisa Ruiz
+                TELEPHONE: 449 198 2734
+                </Text>
+            </Box>
 
-
-
-
-            <div style={{ display: 'flex', justifyContent: 'Middle', marginTop: '50px', marginRight: '300px'}}>
-                <table style={{border: "3px solid rgb(0, 0, 0)"}} width="100%" className="Tablita"  >
+            <div style={{ display: 'flex', justifyContent: 'Middle', marginTop: '50px', marginRight: '300px' }}>
+                <table style={{ border: "1px solid rgb(0, 0, 0)" }} width="100%" className="Tablita"  >
                     <thead>
-                    <tr>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Image </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Doctor </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Hospital </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Date </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Time </th>
-
-
-                    </tr>
+                        <tr>
+                            <th style={{ border: "2px solid rgb(0, 0, 0)" }}> Image </th>
+                            <th style={{ border: "2px solid rgb(0, 0, 0)" }}> Doctor </th>
+                            <th style={{ border: "2px solid rgb(0, 0, 0)" }}> Hospital </th>
+                            <th style={{ border: "2px solid rgb(0, 0, 0)" }}> Date </th>
+                            <th style={{ border: "2px solid rgb(0, 0, 0)" }}> Time </th>
+                        </tr>
                     </thead>
-                        <tbody>
-                    <tr>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}>  <Image style={{ width: 50, height: 50, marginBottom: 15, alignContent: 'center' }} source={require("./Imagenes/DocJ.jpeg")}/> </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Jonathan Josué Villanueva Guerrero </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Hopital Ojocaliente </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> 23/09/2022 </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> 13:45 </th>
-
-
-                    </tr>
-                    <tr>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}>  <Image style={{ width: 50, height: 50, marginBottom: 15, alignContent: 'center' }} source={require("./Imagenes/Dogtor.jpg")}/> </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Dogtor </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Hopital de la Mujer </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> 26/12/2022 </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> 10:35 </th>
-
-
-                    </tr>
-                    <tr>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}>  <Image style={{ width: 50, height: 50, marginBottom: 15, alignContent: 'center' }} source={require("./Imagenes/FF7D.jpg")}/> </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> Fulgencio López</th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> ISSSTE </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> 12/04/2022 </th>
-                        <th style={{border: "3px solid rgb(0, 0, 0)"}}> 17:45 </th>
-
-
-                    </tr>
+                    <tbody>
+                        <tr>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}>  <Image style={{ width: 50, height: 50, marginBottom: 15, alignContent: 'center' }} source={require("./Imagenes/DocJ.jpeg")} /> </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> Jonathan Josué Villanueva Guerrero </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> Hopital Ojocaliente </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> 23/09/2022 </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> 13:45 </th>
+                        </tr>
+                        <tr>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}>  <Image style={{ width: 50, height: 50, marginBottom: 15, alignContent: 'center' }} source={require("./Imagenes/Dogtor.jpg")} /> </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> Dogtor </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> Hopital de la Mujer </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> 26/12/2022 </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> 10:35 </th>
+                        </tr>
+                        <tr>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}>  <Image style={{ width: 50, height: 50, marginBottom: 15, alignContent: 'center' }} source={require("./Imagenes/FF7D.jpg")} /> </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> Fulgencio López</th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> ISSSTE </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> 12/04/2022 </th>
+                            <th style={{ border: "1px solid rgb(0, 0, 0)" }}> 17:45 </th>
+                        </tr>
                     </tbody>
                 </table>
             </div>
+            {tab}
+            {tab}
+            {tab}
+            <Button mt="2" colorScheme="indigo" title="Actualizar">
+                Actualizar
+            </Button>          
+
+        </Box>
+    );
+}
 
 
-            <Button mt="2" colorScheme="indigo" title="Menu"  >
-            Actualizar
-          </Button>
+export default () => {
+    return (
+        <NativeBaseProvider>
+            <Center flex={1} px="3">
+                <MedAp />
+            </Center>
+        </NativeBaseProvider>
+    );
 
-
-</Box>
-
-
-        );}
-
-
-        export default () => {
-            return (
-               <NativeBaseProvider>
-                 <Center flex={1} px="3">
-                     <MedAp/>
-                 </Center>
-               </NativeBaseProvider>
-             );
-              
-        };
+};
--- Menu.js
+++ Menu.js
@@ -1,5 +1,8 @@
 import * as React from "react";
-import { Box, Heading, View, VStack, FormControl, Input, Link, Button, HStack, Center, NativeBaseProvider, Image} from "native-base";
+import {
+  Box, Heading, View, VStack, FormControl, Input, Link,
+  Button, HStack, Center, NativeBaseProvider, Image
+} from "native-base";
 import { useNavigation } from '@react-navigation/native';
 import { Text, StyleSheet, Label } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
@@ -7,88 +10,82 @@ import { createDrawerNavigator, DrawerContentScrollView } from "@react-navigatio
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { DrawerNavigation } from "./Stack";
 
-
 const Drawer = createDrawerNavigator()
-
-
-
 const Menu = () => {
   const navigation = useNavigation();
+  return (
+    <Box safeArea p="2" py="8" w="90%" maxW="400">
+      
+      <Heading size="lg" fontWeight="600" _dark={{
+        color: "black"
+      }}>
+        The You Care team welcomes you.
+      </Heading>
 
-    return (
-<Box safeArea p="2" py="8" w="90%" maxW="400">
-        <Heading   size="lg" fontWeight="600"  _dark={{
-            color: "black"
-          }}>
-              The you Care team welcomes you.            
-            </Heading>
-        
+      <FormControl.Label>The people, the technology and this organization are prepared to provide you with comprehensive care that facilitates the rapid use of the medical services that we offer.</FormControl.Label>
 
-          
-            <FormControl.Label>The people, the technology and this organization are prepared to provide you with comprehensive care that facilitates the rapid use of the medical services that we offer.</FormControl.Label>
+      <Heading size="sm" fontWeight="600" _dark={{
+        color: "black"
+      }}>
+        <h6> We thank you in advance for your cooperation and wish you a pleasant experience. </h6>
 
-            <Heading   size="sm" fontWeight="600"  _dark={{
-            color: "black"
-          }}>
-            <h6> We thank you in advance for your cooperation and wish you a pleasant experience. </h6>
-            
-            </Heading>
+      </Heading>
 
-            <Image style={{ width: 350, height: 180, marginBottom: 15 }} source={require("./Imagenes/Menuimg.jpg")}/>
+      <Image style={{ width: 350, height: 180, marginBottom: 15 }} source={require("./Imagenes/Menuimg.jpg")} />
 
-            <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('MedicalApppoint')} >
-            Medical appointment
-          </Button>
+      <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('MedicalApppoint')} >
+        Medical appointment
+      </Button>
 
-          <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Schedule')} >
-Schedule appointment          </Button>
+      <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Schedule')} >
+        Schedule appointment          </Button>
 
-          <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Catalogue')} >
-            Catalogue
-          </Button>
+      <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Catalogue')} >
+        Catalogue
+      </Button>
 
-          <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('DocSerInf')} >
-            Information about doctors and services
-          </Button>
+      <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('DocSerInf')} >
+        Information about doctors and services
+      </Button>
 
-          <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Prescriptions')} >
-            Prescriptions
-          </Button>
+      <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Prescriptions')} >
+        Prescriptions
+      </Button>
 
-          <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('MedRec')} >
-            Medical record
-          </Button>
+      <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('MedRec')} >
+        Medical record
+      </Button>
 
-          <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Settings')} >
-            Settings
-          </Button>
-          
-          <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Contact')} >
-            About us
-          </Button>
+      <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Settings')} >
+        Settings
+      </Button>
 
-          <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Profile')} >
-            Profile
-          </Button>
-</Box>
+      <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Contact')} >
+        About us
+      </Button>
 
-    );}
+      <Button mt="2" colorScheme="indigo" title="Menu" onPress={() => navigation.navigate('Profile')} >
+        Profile
+      </Button>
+    </Box>
+
+  );
+}
 
 //inicio
 
 //fin
 
-    export default () => {
-       return (
-          <NativeBaseProvider>
-            <Center flex={1} px="3">
-                <Menu/>
-            </Center>
-          </NativeBaseProvider>
-        );
-         
-   };
+export default () => {
+  return (
+    <NativeBaseProvider>
+      <Center flex={1} px="3">
+        <Menu />
+      </Center>
+    </NativeBaseProvider>
+  );
+
+};
 
 
-  
-        
+
